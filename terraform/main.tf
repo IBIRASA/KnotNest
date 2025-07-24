74c3eaef679cb30a94dbaf92477b5c782eb33bda
IBIRASA <d.ishimwe@alustudent.com>
2025-07-24 20:50:51 +0200

    created two azure resources using terraform

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "knotnest-rg"
+  location = "East US"
+}
+resource "azurerm_container_registry" "acr" {
+  name                = "knotcontaineracr123" # must be globally unique
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
