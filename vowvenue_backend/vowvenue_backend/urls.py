58aee1ef1f112af0b1a5b846d925ed5bbe5bdc44
IBIRASA <d.ishimwe@alustudent.com>
2025-08-06 09:17:24 +0200

    feat: Trigger frontend CI pipeline to test workflow after fixes

--- vowvenue_backend/vowvenue_backend/urls.py
+++ vowvenue_backend/vowvenue_backend/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # vowvenue_backend/urls.py
+
 from django.contrib import admin
 from django.urls import path, include # 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('events.urls')), # T
+    path('api/', include('events.urls')), # 
 ]
