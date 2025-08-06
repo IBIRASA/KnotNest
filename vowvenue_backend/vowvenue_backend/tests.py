af2bc08a82fa69a6722afccde065009884ea6ecc
IBIRASA <d.ishimwe@alustudent.com>
2025-08-06 02:11:35 +0200

    fix: Remove old conflicting tests.py file

--- vowvenue_backend/vowvenue_backend/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase, Client
-
-class APITest(TestCase):
-    def test_api_returns_message(self):
-        client = Client()
-        response = client.get('/api/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {"message": "Hello from Vow Venue backend!"})
